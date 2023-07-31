fe03f4dcc76cd696fc77c147044a49de3093eef9
Jamal <sani48218@gmail.com>
2023-07-31 23:33:25 +0100

    Refractoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Intialize pygame, settings, and screen object
@@ -18,18 +19,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most drawn screen visible.
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, sceen, ship)
 
 run_game()
 
