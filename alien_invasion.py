329a391c710afcdfa975041613ce05dea921d5de
Jamal <sani48218@gmail.com>
2023-10-01 21:31:44 +0100

    Aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,18 +14,21 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
     # Make a ship
     ship = Ship(ai_settings, screen)
     # Make a groupe to store bullets in.
     bullets = Group()
+    aliens = Group()
+
+    #create fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
 
